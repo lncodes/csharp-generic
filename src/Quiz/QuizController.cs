2a989c9dd64c009d15c65ca809a443b0de83dc21
Theodorus Doni <79934036+Theodorus-Doni@users.noreply.github.com>
2024-09-04 20:16:14 +0700

    [200~Update GitHub workflows and documentation (#5)

--- src/Quiz/QuizController.cs
+++ src/Quiz/QuizController.cs
@@ -1,31 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Lncodes.Example.Generic
+namespace Lncodes.Example.Generic;
+
+public sealed class QuizController<TQuestion, TAnswer, TChoicesCollection> where TChoicesCollection : ICollection<TAnswer>
 {
-    public sealed class QuizController<TQuestion, TAnswer, TChoice> where TChoice : ICollection<TAnswer>
+    public TQuestion Question { get; }
+    public TAnswer Answer { get; }
+    public TChoicesCollection ChoiceCollection { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QuizController{TQuestion, TAnswer, TChoicesCollection}"/> class.
+    /// </summary>
+    /// <param name="question">The quiz question.</param>
+    /// <param name="answer">The correct answer to the quiz question.</param>
+    /// <param name="choiceCollection">A collection of possible answers.</param>
+    public QuizController(TQuestion question, TAnswer answer, TChoicesCollection choiceCollection) =>
+        (Question, Answer, ChoiceCollection) = (question, answer, choiceCollection);
+
+    /// <summary>
+    /// Checks if the provided answer matches the correct answer.
+    /// </summary>
+    /// <param name="answer">The user's answer.</param>
+    public void CheckAnswer(TAnswer answer)
     {
-        public readonly TAnswer Answer;
-        public readonly TQuestion Question;
-        public readonly TChoice ChoiceCollection;
-
-        // Constructor
-        public QuizController(TQuestion question, TAnswer answer, TChoice choiceCollection) =>
-            (Question, Answer, ChoiceCollection) = (question, answer, choiceCollection);
-
-        /// <summary>
-        /// Method to Check a Answer
-        /// </summary>
-        /// <param name="answer">User Answer</param>
-        public void CheckAnswer(TAnswer answer)
+        if (answer.Equals(Answer))
+            Console.WriteLine("Your answer is correct.");
+        else
         {
-            if (answer.Equals(Answer))
-                Console.WriteLine("Answer correct");
-            else
-            {
-                Console.WriteLine("Answer wrong");
-                Console.WriteLine($"The correct answer is {this.Answer}");
-            }
+            Console.WriteLine("Your answer is incorrect.");
+            Console.WriteLine($"The correct answer is: {this.Answer}");
         }
     }
 }
